0e931ee950cd27f631bae0469e21323a6441967b
Sandeep Dalal <sandeepdalal1995@gmail.com>
2020-05-31 00:01:13 +0530

    Adding app.js and Using Ionic Elements like "Normal" HTML Elements

---
+++ app.js
@@ -0,0 +1,8 @@
+const reasonInput = document.querySelector("#input-reason");
+const amountInput = document.querySelector("#input-amount");
+const cancelBtn = document.querySelector("#btn-cancel");
+const confirmBtn = document.querySelector("#btn-confirm");
+
+confirmBtn.addEventListener("click", () => {
+  console.log("It works!");
+});
