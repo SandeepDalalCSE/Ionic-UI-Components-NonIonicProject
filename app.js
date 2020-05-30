bb50629eae4ecb9955fbc97643161ffe787e715a
Sandeep Dalal <sandeepdalal1995@gmail.com>
2020-05-31 00:25:11 +0530

    adding a new row for total expenses. and updating it using app.js

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const amountInput = document.querySelector("#input-amount");
 const cancelBtn = document.querySelector("#btn-cancel");
 const confirmBtn = document.querySelector("#btn-confirm");
 const expensesList = document.querySelector("#expenses-list");
+const totalExpensesOutput = document.querySelector("#total-expenses");
+
+let totalExpenses = 0;
 
 const clear = () => {
   reasonInput.value = "";
@@ -24,6 +27,10 @@ confirmBtn.addEventListener("click", () => {
   newItem.textContent = enteredReason + ": Rs" + enteredAmount;
 
   expensesList.appendChild(newItem);
+
+  totalExpenses += +enteredAmount;
+  totalExpensesOutput.textContent = totalExpenses;
+
   clear();
 });
 
